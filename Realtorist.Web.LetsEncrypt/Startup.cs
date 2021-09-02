d06485d5ad663be5316fc007868277df00ad7639
Dmytro Bondarchuk <dmytro.bondarchuk@hootsuite.com>
2021-09-02 13:25:35 -0400

    Added https redirection

--- Realtorist.Web.LetsEncrypt/Startup.cs
+++ Realtorist.Web.LetsEncrypt/Startup.cs
@@ -1,5 +1,6 @@
 using ExtCore.Infrastructure.Actions;
 using LettuceEncrypt;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,7 +14,7 @@ using System.IO;
 
 namespace Realtorist.Web.LetsEncrypt
 {
-    public class Startup : IConfigureServicesAction
+    public class Startup : IConfigureServicesAction, IConfigureAction
     {
         public int Priority => 5;
 
@@ -23,34 +24,45 @@ namespace Realtorist.Web.LetsEncrypt
             var configuration = serviceProvider.GetService<IConfiguration>();
             var settingsProvider = serviceProvider.GetService<ISettingsProvider>();
             var logger = serviceProvider.GetService<ILogger<Startup>>();
-            
+
             var settings = settingsProvider.GetSettingAsync<WebsiteSettings>(SettingTypes.Website).Result;
             var profile = settingsProvider.GetSettingAsync<ProfileSettings>(SettingTypes.Profile).Result;
+
+            var domainName = settings?.WebsiteAddress;
+            if (env.IsDevelopment() && !configuration["LetsEncrypt:DomainName"].IsNullOrEmpty())
+            {
+                domainName = configuration["LetsEncrypt:DomainName"];
+            }
+
+            if (domainName.IsNullOrEmpty())
+            {
+                logger.LogWarning($"Won't use LetsEncrypt. Domain name wasn't set neither through website address in setting nor through configuration (LestEncrypt->DomainName property)");
+                return;
+            }
+            
             services.AddLettuceEncrypt(option =>
             {
-                if (settings?.WebsiteAddress.IsNullOrEmpty() ?? true) return;
-
+                option.EmailAddress = profile.Email;
+                option.DomainNames = env.IsDevelopment() ? new[] { domainName } : new[] { domainName, $"www.{domainName}" };
                 option.AcceptTermsOfService = true;
+
                 if (env.IsDevelopment())
                 {
-                    var domainName = configuration["LetsEncrypt:DomainName"];
-                    if (domainName.IsNullOrEmpty()) 
-                    {
-                        logger.LogWarning("Domain name for lets encrypt is not set");
-                        return;
-                    }
-
-                    option.DomainNames = new[] { domainName };
                     option.UseStagingServer = true;
                 }
-                else
-                {
-                    option.DomainNames = new[] { settings.WebsiteAddress, $"www.{settings.WebsiteAddress}" };
-                }
-
-                option.EmailAddress = profile.Email;
             })
             .PersistDataToDirectory(new DirectoryInfo("certs"), "password");
+        }
+
+        void IConfigureAction.Execute(IApplicationBuilder app, IServiceProvider serviceProvider)
+        {
+            var env = serviceProvider.GetService<IWebHostEnvironment>();
+            if (!env.IsDevelopment())
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
         }
     }
 }
